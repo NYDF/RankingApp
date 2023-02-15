3f853c0578650741e0c4ec26a20ecae30e3833fa
DF Song <yusu.us@gmail.com>
2023-02-15 16:04:01 -0500

    working on react

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,13 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const RankItems = () => {
+    const [items, setItems] = useState([])
+
+    return (
+        <main>
+            (items != null)?<div>
+
+            </div>
+        </main>
+        )
+}
